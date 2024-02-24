d8c27cbf26e336ea0eb03770cbe19e063d0b9c93
Veronelli <facu2000veronelli@outlook.com>
2024-02-24 20:09:44 +0000

    add pre-commit

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+Root configuration
+"""
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,4 +9,10 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    """
+    Root of endpoints, to know the health of the project
+
+    Returns:
+        dict: message attribute with 'Hello World'
+    """
     return {"message": "Hello World"}
