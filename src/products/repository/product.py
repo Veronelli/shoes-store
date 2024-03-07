bfe9ef81519c81192c2f532d2282311af2eb8757
Veronelli <facu2000veronelli@outlook.com>
2024-03-07 12:32:50 +0000

    add mocker and mypy

---
+++ src/products/repository/product.py
@@ -0,0 +1,11 @@
+"""
+Repository for product operations
+"""
+
+
+async def get_product_list() -> list[str]:
+    """
+    Returns:
+        list(Product): list of products
+    """
+    return []
---
+++ tests/unit/functionality/test_product_graphQL.py
@@ -0,0 +1,20 @@
+"""
+Test integration using GraphQL in product module
+"""
+
+import pytest
+from pytest_mock import MockerFixture
+from src.products.repository.product import get_product_list
+
+
+@pytest.mark.asyncio
+async def test_get_products_from_graphql(mocker: MockerFixture):
+    """
+    Test obtain a list of product, integrated with graphQL
+
+    Fixtures:
+    -   mocker: object that allow to modify the normal behavior
+    """
+    products = await get_product_list()
+
+    assert len(products) > 0
