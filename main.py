56b34e0cb94681aefeb26028b90237a0860d9ccf
Veronelli <facu2000veronelli@outlook.com>
2024-03-25 02:44:01 +0000

    add strawberry and implement asgi

--- main.py
+++ main.py
@@ -2,38 +2,12 @@
 Root configuration
 """
 
-from contextlib import asynccontextmanager
-from typing import Any, AsyncGenerator
 from fastapi import FastAPI
-from src.products.schema import (
-    QueryProduct,
-    CreateProduct,
-    DeleteProduct,
-    UpdateProduct,
-)
-from src.database import get_db
-from starlette_graphene3 import GraphQLApp
-
-
-@asynccontextmanager  # type: ignore
-async def lifespan(app: FastAPI) -> AsyncGenerator[FastAPI, Any]:
-    """
-    Definition of lifestan to create a connection to the server
-    Args:
-    -   app:(FastAPI) instance of fastapi
-    """
-    await get_db.connect()  # type: ignore
-    yield
-    await get_db.disconnect()  # type: ignore
-
+from src.products.schema import graphql_app
 
 app = FastAPI()
-app.add_route(
-    "/product",
-    GraphQLApp(
-        schema=QueryProduct, mutations=[UpdateProduct, CreateProduct, DeleteProduct]
-    ),
-)
+
+app.add_route("/graphql", graphql_app)
 
 
 @app.get("/")
--- src/products/schema.py
+++ src/products/schema.py
@@ -1,176 +1,61 @@
 """
-Define the GraphQL schema for our API
+Modeling schema to interact to DB
+
+TODO:
+-   continue implementing GraphQL to understand
 """
 
-from typing import Optional
-from graphene import ID, Field, ObjectType, Int, ResolveInfo, String
-from graphene_sqlalchemy import SQLAlchemyObjectType
-from src.database import get_db
-from src.commons.exceptions import NotFoundItemQuery
-from src.products.db_model import ProductDB
+from dataclasses import dataclass
+from typing import List, Optional
+from strawberry import Schema, type, field
+from strawberry.asgi import GraphQL
 
 
-class Product(SQLAlchemyObjectType):
+@dataclass
+@type
+class Product:
     """
-    A class represents Product schema
-    """
+    A class represents a product modeling for data base
 
-    class Meta:
-        """
-        Meta class to config schema
-        """
-
-        model = ProductDB
-        interface = ObjectType
-
-
-class QueryProduct(ObjectType):
-    """
-    A class represents query for products
-    """
-
-    products = Field(lambda: list[Product], limit=Int())
-
-    def resolve_product(
-        self, info: ResolveInfo, limit: Optional[int] = None
-    ) -> list[Product]:
-        """
-        Resolve list product
-        Args:
-        -   self:(Self)
-        -   info:(ResolveInfo): provides contextual information
-            about the current GraphQL resolution process
-        -   limit:(Optional[int]) limit list of products
-
-        Returns:
-        -   (List[Product]): list of products
-        """
-        query = Product.get_query(info)
-        if limit:
-            return query.limit(limit).all()
-        return query.all()
-
-
-class CreateProduct(ObjectType):
-    """
-    A class represent schema to create a product
-    """
-
-    product = Field(lambda: Product)
-
-    class Arguments:
-        """
-        Argument class to fill data
-        Attributes:
-        -   name:(String*) name of product
-        -   description:(String*) description of product
-        -   specs:(Integer) spec id of product
-        -   categories:(List[Integer]) categories id of product
-        -   tags:(list[Integer]) tags id of product
-        TODO:
-        -   Create integration test for this cases
-        -   Complete model after create their integration test.
-        """
-
-        name = String(require=True)
-        description = String()
-
-    def mutate(self, info: ResolveInfo, name: str, description: str) -> Product:
-        """
-        Create new product
-        Args:
-        -   info:(ResolveInfo) provides contextual information
-        -   name:(str) name of product
-        -   description:(str) description of product
-        Returns:
-        -   (Product): new product created
-
-        TODO:
-        -   fill with all product attributes
-        """
-        db = get_db()
-        product = ProductDB(name=name, description=description)
-        db.add(product)
-        db.commit()
-        db.refresh()
-        return CreateProduct(product=product)
-
-
-class UpdateProduct(ObjectType):
-    """
-    Represent a class to update product
-    """
-
-    class Arguments:
-        """
-        Arguments class to fill data
-        Attributes:
-        -   id:(Integer) product id
-        -   name:(String) product name
-        -   description:(String) product description
-        -   specs:(Integer) product spec id
-        -   categories:(list[Integer]) product categories
-        -   tags:(list[Integer]) product tags id
-        """
-
-        id = ID(required=True)
-        name = String()
-        description = String()
-
-    def mutate(self, info: ResolveInfo, id: ID, name: str, description: str) -> Product:
-        """
-        update a product by id
-        Args:
-        -   info:(ResolveInfo) provides contextual information
-        -   id:(ID) product id
-        -   name:(str) name of product
-        -   description:(str) description of product
-        Returns:
-        -   (UpdateProduct): Product with updated data
-        """
-        db = get_db()
-        product = db.query(ProductDB).filter(ProductDB.id == id).first()
-
-        if not product:
-            raise NotFoundItemQuery("Product not found")
-
-        if name:
-            product.name = name
-        if description:
-            product.description = description
-
-        db.commit()
-        db.refresh(product)
-        return UpdateProduct(product=product)
-
-
-class DeleteProduct(ObjectType):
-    """
-    A class represent schema for product
     Attributes:
-    -   success:(String) result of delte
+    -   id:(Integer) id of product
+    -   name:(String*) name of product
+    -   description:(String*) description of product
+    -   spec:(SpecDB) specs of product
+    -   category:(CategoryDB) category of product
+    -   tags: (list[TagDB]) tag list of product
+    -   visibility:(bool) allow to show to customers
     """
 
-    success = Field(lambda: String)
+    id: int
+    name: Optional[str] = None
+    description: Optional[str] = None
+    visibility: Optional[bool] = None
+    spec: Optional[int] = None
+    tags: Optional[int] = None
+    cateogry: Optional[int] = None
 
-    class Arguments:
-        """
-        Represent class arguments to use
-        -   id:(ID) id of product
-        """
 
-        id = ID(required=True)
+@type
+class Query:
+    """
+    A class to make a query
+    """
 
-    def mutate(self, info: ResolveInfo, id: ID) -> "DeleteProduct":
+    @field
+    def products(self) -> List[Product]:
         """
-        Delete product mutation
-        -   info:(ResolveInfo) provides contextual information
-        -   id:(ID) product id
+        Field of products list
+
+        Returns:
+        -   List[Product]: List of products
         """
-        db = get_db()
-        product = db.query(ProductDB).filter(ProductDB.id == id).first()
-        if not product:
-            raise NotFoundItemQuery()
-        db.delete(product)
-        db.commit()
-        return DeleteProduct(success=f"Product {id} deleted successfully")
+        product = Product(
+            id=1, name="Product 1", description="Descrition about product 2"
+        )
+
+        return [product]
+
+
+schema = Schema(query=Query)
+graphql_app = GraphQL(schema)
